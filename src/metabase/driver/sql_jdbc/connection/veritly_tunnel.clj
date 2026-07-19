df6fd2d650721d396110a0951d0daf605c9f916a
Denis Barzanov <denis.barzanov2002@gmail.com>
2026-07-19 10:45:08 +0300

    feat: bind veritly connector routes from database form

--- src/metabase/driver/sql_jdbc/connection/veritly_tunnel.clj
+++ src/metabase/driver/sql_jdbc/connection/veritly_tunnel.clj
@@ -1,10 +1,12 @@
 (ns metabase.driver.sql-jdbc.connection.veritly-tunnel
   "A local TCP entrance backed by authenticated, outbound WebSocket streams through a Veritly connector."
   (:require
+   [cheshire.core :as json]
+   [clojure.string :as str]
    [metabase.util.log :as log])
   (:import
    (java.net InetAddress ServerSocket Socket URI URLEncoder)
-   (java.net.http HttpClient WebSocket WebSocket$Listener)
+   (java.net.http HttpClient HttpRequest HttpRequest$BodyPublishers HttpResponse$BodyHandlers WebSocket WebSocket$Listener)
    (java.nio ByteBuffer)
    (java.time Duration)
    (java.util.concurrent CompletableFuture CompletionStage ExecutorService Executors TimeUnit)))
@@ -34,6 +36,29 @@
   (let [gateway (:veritly-gateway details)
         route   (URLEncoder/encode ^String (:veritly-route details) "UTF-8")]
     (URI/create (str gateway (if (.contains ^String gateway "?") "&" "?") "route=" route))))
+
+(defn- env [key]
+  (let [value (some-> (System/getenv key) str/trim)]
+    (when (str/blank? value)
+      (throw (ex-info (str key " is required for Veritly connector routes") {:key key})))
+    value))
+
+(defn- bind! [details]
+  (let [base    (str/replace (env "VERITLY_CONTROL_URL") #"/+$" "")
+        payload (json/generate-string {:route (:veritly-route details)
+                                       :token (:veritly-token details)
+                                       :host  (:host details)
+                                       :port  (:port details)})
+        request (-> (HttpRequest/newBuilder (URI/create (str base "/connector/internal/route")))
+                    (.header "Authorization" (str "Bearer " (env "VERITLY_CONTROL_TOKEN")))
+                    (.header "Content-Type" "application/json")
+                    (.timeout (Duration/ofSeconds 5))
+                    (.POST (HttpRequest$BodyPublishers/ofString payload))
+                    (.build))
+        response (.send client request (HttpResponse$BodyHandlers/ofString))]
+    (when-not (= 200 (.statusCode response))
+      (throw (ex-info "Veritly connector route could not be bound"
+                      {:status (.statusCode response)})))))
 
 (defn- bridge! [tracker details ^Socket socket]
   (let [output (.getOutputStream socket)
@@ -95,18 +120,20 @@
          (string? (:veritly-gateway details))]}
   (if (and (enabled? details) (open? details))
     details
-    (let [server   (ServerSocket. 0 128 (InetAddress/getLoopbackAddress))
-          executor (Executors/newCachedThreadPool)
-          tracker  {:server server :executor executor :clients (atom #{})}
-          port     (.getLocalPort server)
-          next     (assoc details
-                          :host "localhost"
-                          :orig-port (:port details)
-                          :port port
-                          :veritly-tunnel-tracker tracker)]
-      (.submit ^ExecutorService executor ^Runnable (fn [] (accept! tracker details)))
-      (log/tracef "Opened Veritly connector route %s on local port %d" (:veritly-route details) port)
-      next)))
+    (do
+      (bind! details)
+      (let [server   (ServerSocket. 0 128 (InetAddress/getLoopbackAddress))
+            executor (Executors/newCachedThreadPool)
+            tracker  {:server server :executor executor :clients (atom #{})}
+            port     (.getLocalPort server)
+            next     (assoc details
+                            :host "localhost"
+                            :orig-port (:port details)
+                            :port port
+                            :veritly-tunnel-tracker tracker)]
+        (.submit ^ExecutorService executor ^Runnable (fn [] (accept! tracker details)))
+        (log/tracef "Opened Veritly connector route %s on local port %d" (:veritly-route details) port)
+        next))))
 
 (defn close! [details]
   (when-let [tracker (:veritly-tunnel-tracker details)]
